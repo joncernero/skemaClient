31124d6fc9264f1f7ed2c054926f53ead38dc376
Michael Crenshaw <mkrenshaw96@gmail.com>
2025-09-23 19:01:56 -0400

    Refactor and remove sub route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
-import Page from '@/app/(rs)/home/page';
+import Hero from '@/components/Hero'
+import About from '@/components/About'
+import Reviews from '@/components/Reviews'
+import Pricing from '@/components/Pricing'
+import ScrollToTop from '@/components/ui/ScrollToTop'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
-export default Page;
+export const metadata = {
+  title: 'Home'
+}
+
+export default function Home() {
+  return (
+    <>
+      <div className='mx-auto w-full max-w-full'>
+        <ScrollToTop />
+        <Header />
+        <Hero />
+        <About />
+        <Pricing />
+        <Reviews />
+        <Footer />
+      </div>
+    </>
+  )
+}
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,25 +1,25 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react'
+import Image from 'next/image'
 
 const About = () => {
   return (
-    <section className="about py-10 sm:py-16 lg:py-24 bg-gradient-to-b from-[#439cfb] via-[#f187fb] to-white">
-      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div className="max-w-2xl mx-auto text-center">
-          <h2 className="text-3xl font-bold leading-tight sm:text-4xl lg:text-5xl">
+    <section className='about py-10 sm:py-16 lg:py-24 bg-gradient-to-b from-[#439cfb] via-[#f187fb] to-white'>
+      <div className='px-4 mx-auto max-w-7xl sm:px-6 lg:px-8'>
+        <div className='max-w-2xl mx-auto text-center'>
+          <h2 className='text-3xl font-bold leading-tight sm:text-4xl lg:text-5xl'>
             How does it work?
           </h2>
-          <p className="max-w-lg mx-auto mt-4 text-base leading-relaxed">
+          <p className='max-w-lg mx-auto mt-4 text-base leading-relaxed'>
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
             sint. Velit officia consequat duis.
           </p>
         </div>
-        <div className="relative mt-12 lg:mt-20">
-          <div className="absolute inset-x-0 hidden xl:px-44 top-2 md:block md:px-20 lg:px-28">
+        <div className='relative mt-12 lg:mt-20'>
+          <div className='absolute inset-x-0 hidden xl:px-44 top-2 md:block md:px-20 lg:px-28'>
             <Image
-              className="w-full"
-              src="https://cdn.rareblocks.xyz/collection/celebration/images/steps/2/curved-dotted-line.svg"
-              alt="dottedline"
+              className='w-full'
+              src='https://cdn.rareblocks.xyz/collection/celebration/images/steps/2/curved-dotted-line.svg'
+              alt='dottedline'
               width={0}
               height={0}
               priority
@@ -36,41 +36,41 @@ const About = () => {
         /> */}
           </div>
 
-          <div className="relative grid grid-cols-1 text-center gap-y-12 md:grid-cols-3 gap-x-12">
+          <div className='relative grid grid-cols-1 text-center gap-y-12 md:grid-cols-3 gap-x-12'>
             <div>
-              <div className="flex items-center justify-center w-16 h-16 mx-auto bg-white border-2 border-gray-200 rounded-full shadow">
-                <span className="text-xl font-semibold text-gray-700"> 1 </span>
+              <div className='flex items-center justify-center w-16 h-16 mx-auto bg-white border-2 border-gray-200 rounded-full shadow'>
+                <span className='text-xl font-semibold text-gray-700'> 1 </span>
               </div>
-              <h3 className="mt-6 text-xl font-semibold leading-tight text-black md:mt-10">
+              <h3 className='mt-6 text-xl font-semibold leading-tight text-black md:mt-10'>
                 Create a free account
               </h3>
-              <p className="mt-4 text-base text-gray-600">
+              <p className='mt-4 text-base text-gray-600'>
                 Amet minim mollit non deserunt ullamco est sit aliqua dolor do
                 amet sint. Velit officia consequat duis enim velit mollit.
               </p>
             </div>
 
             <div>
-              <div className="flex items-center justify-center w-16 h-16 mx-auto bg-white border-2 border-gray-200 rounded-full shadow">
-                <span className="text-xl font-semibold text-gray-700"> 2 </span>
+              <div className='flex items-center justify-center w-16 h-16 mx-auto bg-white border-2 border-gray-200 rounded-full shadow'>
+                <span className='text-xl font-semibold text-gray-700'> 2 </span>
               </div>
-              <h3 className="mt-6 text-xl font-semibold leading-tight text-black md:mt-10">
+              <h3 className='mt-6 text-xl font-semibold leading-tight text-black md:mt-10'>
                 Build your website
               </h3>
-              <p className="mt-4 text-base text-gray-600">
+              <p className='mt-4 text-base text-gray-600'>
                 Amet minim mollit non deserunt ullamco est sit aliqua dolor do
                 amet sint. Velit officia consequat duis enim velit mollit.
               </p>
             </div>
 
             <div>
-              <div className="flex items-center justify-center w-16 h-16 mx-auto bg-white border-2 border-gray-200 rounded-full shadow">
-                <span className="text-xl font-semibold text-gray-700"> 3 </span>
+              <div className='flex items-center justify-center w-16 h-16 mx-auto bg-white border-2 border-gray-200 rounded-full shadow'>
+                <span className='text-xl font-semibold text-gray-700'> 3 </span>
               </div>
-              <h3 className="mt-6 text-xl font-semibold leading-tight text-black md:mt-10">
+              <h3 className='mt-6 text-xl font-semibold leading-tight text-black md:mt-10'>
                 Release & Launch
               </h3>
-              <p className="mt-4 text-base text-gray-600">
+              <p className='mt-4 text-base text-gray-600'>
                 Amet minim mollit non deserunt ullamco est sit aliqua dolor do
                 amet sint. Velit officia consequat duis enim velit mollit.
               </p>
@@ -79,7 +79,7 @@ const About = () => {
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default About;
+export default About
