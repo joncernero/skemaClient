e2ab15898ddb98ae9646202dbb843afb8073caed
Jonathan Cernero <cernero.jon@gmail.com>
2026-07-12 11:33:04 -0400

    Adding Google Tags to Root Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,6 +12,8 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://blackwrendigital.com"),
@@ -52,6 +55,7 @@ export default function RootLayout({
           Skip to main content
         </a>
         {children}
+        <GoogleAnalytics gaId="G-D40DV20ZCB" />
       </body>
     </html>
   );
